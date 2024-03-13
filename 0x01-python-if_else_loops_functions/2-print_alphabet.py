41a7ad2cb675207067f51e690a24c0be2b0f7b24
Obolaji <obolaji.anibaba@gmail.com>
2024-03-12 20:16:18 -0700

    0x01 python

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for a in range('a', 'z')
+for a in range('a', 'z'):
 print ("{}".format(a)), end='')
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for a in range('a', 'z'):
+    if (a) is not 'q' and 'a'is not 'e':
+        print("{}".format(a), end='')
